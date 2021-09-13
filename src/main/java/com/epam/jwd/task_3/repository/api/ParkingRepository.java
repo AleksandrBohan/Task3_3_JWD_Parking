d4da6219c717b3f888c1dff65b25080ee3c24f03
Alex_Boh <aleksandrbohan2@gmail.com>
2021-09-13 16:05:03 +0300

    I finish api and Iml packages of repository.

--- src/main/java/com/epam/jwd/task_3/repository/api/ParkingRepository.java
+++ src/main/java/com/epam/jwd/task_3/repository/api/ParkingRepository.java
@@ -1,11 +1,13 @@
 package com.epam.jwd.task_3.repository.api;
 
 import com.epam.jwd.task_3.repository.model.Car;
+import com.epam.jwd.task_3.repository.model.ParkingPlace;
 
 import java.util.List;
 
 public interface ParkingRepository {
 
    void fillEveryCarById(List<Car> cars);
+   void fillEveryParkingPlaceById(List<ParkingPlace> parkingPlaces);
 
 }
--- src/main/java/com/epam/jwd/task_3/repository/impl/ParkingRepositoryImpl.java
+++ src/main/java/com/epam/jwd/task_3/repository/impl/ParkingRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.epam.jwd.task_3.repository.impl;
 
 import com.epam.jwd.task_3.repository.api.ParkingRepository;
 import com.epam.jwd.task_3.repository.model.Car;
+import com.epam.jwd.task_3.repository.model.ParkingPlace;
 
 import java.util.List;
 
@@ -9,10 +10,20 @@ public class ParkingRepositoryImpl implements ParkingRepository {
 
     @Override
     public void fillEveryCarById(List<Car> cars) {
-
         for (int i = 0; i < cars.size(); i++) {
                 cars.get(i).setPersonalNumberOfCar(i+1l);
+
             }
 
     }
+
+    @Override
+    public void fillEveryParkingPlaceById(List<ParkingPlace> parkingPlaces) {
+        for (int i = 0; i < parkingPlaces.size(); i++) {
+            parkingPlaces.get(i).setIndexOfPlace(i+1);
+
+        }
+
+    }
+
 }
