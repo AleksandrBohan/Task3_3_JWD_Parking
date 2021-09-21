ccbe0ffdaf5091aac98b46951cce1696041c651c
Alex_Boh <aleksandrbohan2@gmail.com>
2021-09-21 15:09:14 +0300

    I edit PersonalNumberGenerator class in logic package of services.

--- src/main/java/com/epam/jwd/task_3/services/logic/PersonalNumberGenerator.java
+++ src/main/java/com/epam/jwd/task_3/services/logic/PersonalNumberGenerator.java
@@ -1,11 +1,18 @@
 package com.epam.jwd.task_3.services.logic;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PersonalNumberGenerator {
 
     private static Long personalNumber = 999L;
 
-    public static Long generateId() {
-        return personalNumber++;
+    private ReentrantLock lock = new ReentrantLock();
+
+    public Long generateId() {
+        lock.lock();
+        personalNumber++;
+        lock.unlock();
+        return personalNumber;
     }
 
 }
