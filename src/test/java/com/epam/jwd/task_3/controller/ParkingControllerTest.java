bf582f42cde095c97f166a9a11178794ba9c3c99
Alex_Boh <aleksandrbohan2@gmail.com>
2021-09-26 22:30:35 +0300

    I create checkCountOfParkingPlacesNumberAfterInput() and  checkCountOfCarsAfterInput() in ParkingControllerTest class in test package.

--- src/test/java/com/epam/jwd/task_3/controller/ParkingControllerTest.java
+++ src/test/java/com/epam/jwd/task_3/controller/ParkingControllerTest.java
@@ -10,29 +10,41 @@ class ParkingControllerTest {
 
     ParkingController parkingController;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
     @Test
     void checkCountOfCarsAfterInput() {
-        int [] numbersOfCars = {1,2,3,4,5};
+        int [] carsCount = {1,2,3,4,5};
         parkingController = new ParkingController();
 
         assertAll("numbersOfCars",
-                () -> assertEquals(numbersOfCars[0], parkingController.getCarNumberAfterInput("1")),
-                () -> assertEquals(numbersOfCars[1], parkingController.getCarNumberAfterInput("2")),
-                () -> assertEquals(numbersOfCars[2], parkingController.getCarNumberAfterInput("3")),
-                () -> assertEquals(numbersOfCars[3], parkingController.getCarNumberAfterInput("4")),
-                () -> assertEquals(numbersOfCars[4], parkingController.getCarNumberAfterInput("5"))
+                () -> assertEquals(carsCount[0], parkingController
+                        .getCarNumberAfterInput("1")),
+                () -> assertEquals(carsCount[1], parkingController
+                        .getCarNumberAfterInput("2")),
+                () -> assertEquals(carsCount[2], parkingController
+                        .getCarNumberAfterInput("3")),
+                () -> assertEquals(carsCount[3], parkingController
+                        .getCarNumberAfterInput("4")),
+                () -> assertEquals(carsCount[4], parkingController
+                        .getCarNumberAfterInput("5"))
         );
     }
 
     @Test
-    void getParkingPlacesNumberAfterInput() {
+    void checkCountOfParkingPlacesNumberAfterInput() {
+        int [] numbersOfCars = {5,10,15,20,25};
+        parkingController = new ParkingController();
+
+        assertAll("numbersOfCars",
+                () -> assertEquals(numbersOfCars[0], parkingController
+                        .getParkingPlacesNumberAfterInput("1")),
+                () -> assertEquals(numbersOfCars[1], parkingController
+                        .getParkingPlacesNumberAfterInput("2")),
+                () -> assertEquals(numbersOfCars[2], parkingController
+                        .getParkingPlacesNumberAfterInput("3")),
+                () -> assertEquals(numbersOfCars[3], parkingController
+                        .getParkingPlacesNumberAfterInput("4")),
+                () -> assertEquals(numbersOfCars[4], parkingController
+                        .getParkingPlacesNumberAfterInput("5"))
+        );
     }
 }
