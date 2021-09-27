9295bf3dec96742878ed03e96b9f339059f6564a
Alex_Boh <aleksandrbohan2@gmail.com>
2021-09-27 20:29:49 +0300

    I create logger cover in CarServiceImpl class.

--- src/main/java/com/epam/jwd/task_3/services/impl/CarServiceImpl.java
+++ src/main/java/com/epam/jwd/task_3/services/impl/CarServiceImpl.java
@@ -4,30 +4,35 @@ import com.epam.jwd.task_3.repository.impl.CarRepositoryImpl;
 import com.epam.jwd.task_3.repository.model.Car;
 import com.epam.jwd.task_3.services.api.CarFactory;
 import com.epam.jwd.task_3.services.api.CarService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 
 public class CarServiceImpl implements CarService {
 
+    private static final Logger logger = LogManager.getLogger(CarServiceImpl.class);
+
     @Override
     public List<Car> fillCarListForParking(CarFactory factory, int countOfCars, List<Car> carStorage) {
 
         for (int i = 0; i < countOfCars; i++){
+            logger.info("Cars list: \n");
             new CarRepositoryImpl().save(factory.getMercedesCar(), carStorage);
-            System.out.println(carStorage.get(0));
+            logger.debug(carStorage.get(0));
 
             new CarRepositoryImpl().save(factory.getToyotaCar(), carStorage);
-            System.out.println(carStorage.get(1));
+            logger.debug(carStorage.get(1));
 
             new CarRepositoryImpl().save(factory.getOpelCar(), carStorage);
-            System.out.println(carStorage.get(2));
+            logger.debug(carStorage.get(2));
 
             new CarRepositoryImpl().save(factory.getVolkswagenCar(), carStorage);
-            System.out.println(carStorage.get(3));
+            logger.debug(carStorage.get(3));
 
             new CarRepositoryImpl().save(factory.getRenaultCar(), carStorage);
-            System.out.println(carStorage.get(4));
+            logger.debug(carStorage.get(4));
 
         }
 
